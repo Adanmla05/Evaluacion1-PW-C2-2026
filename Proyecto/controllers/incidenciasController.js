7273f3658e2159b761426fa85247cef8b3948e97
JenniferC57 <00160223@uca.edu.sv>
2026-09-17 20:16:45 -0600

    Añadiendo cambio de estado y eliminacion de incidencias

--- Proyecto/controllers/incidenciasController.js
+++ Proyecto/controllers/incidenciasController.js
@@ -51,6 +51,91 @@ const registrarIncidencia = (req, res) => {
     });
 };
 
+const cambiarEstadoIncidencia = (req, res) => {
+    const id = Number(req.params.id);
+    const { estado } = req.body;
+
+    //Validacion que el ID sea un numero entero positivo
+    if (!Number.isInteger(id) || id <= 0) {
+        return res.status(400).json({
+            mensaje: "El ID proporcionado no es valido"
+        });
+    }
+
+    //Busqueda de incidencia por su ID
+    const incidenciaEncontrada = incidencias.find(
+        (incidencia) => incidencia.id === id
+    );
+
+    if (!incidenciaEncontrada) {
+        return res.status(404).json({
+            mensaje: "Incidencia no encontrada"
+        });
+    }
+
+    if (!estado || typeof estado !== "String" || estado.trim() === ""){
+        return res.status(400).json({
+            mensaje: "El estado es obligatorio"
+        });
+    }
+
+    switch (estado.trim().toLowerCase()){
+        case "pendiente":
+            incidenciaEncontrada.estado = "Pendiente";
+            break;
+        case "en proceso":
+            incidenciaEncontrada.estado = "En proceso";
+            break;
+        case "resuelta":
+            incidenciaEncontrada.estado = "Resuelta";
+            break;
+        case "cancelada":
+            incidenciaEncontrada.estado = "Cancelada";
+            break;
+        default:
+            return res.status(400).json({
+                mensaje: "El estado debe ser Pendiente, En proceso, Resuelta o Cancelada"
+            });
+    }
+
+    return res.status(200).json({
+        mensaje: "Estado actualizado correctamente",
+        incidencia: incidenciaEncontrada
+    });
+};
+
+//Eliminar incidencia
+const eliminarIncidencia = (req, res)  => {
+    const id = Number(req.params.id);
+
+    if (!Number.isInteger(id) || id <= 0) {
+        return res.status(400).json({
+            mensaje: "El ID proporcionado no es valido"
+        });
+    }
+
+    const indiceIncidencia = incidencias.findIndex(
+        (incidencia) => incidencia.id === id
+    );
+
+    if(indiceIncidencia === -1){
+        return res.status(404).json({
+            mensaje: "Incidencia no encontrada"
+        });
+    }
+
+    const incidenciaEliminada =incidencias.splice(
+        indiceIncidencia, 1
+    )[0];
+
+    return res.status(200).json({
+        mensaje: "Incidencia eliminada correctamente",
+        incidencia: incidenciaEliminada
+    });
+};
+
 module.exports = {
-    registrarIncidencia
+    registrarIncidencia,
+    cambiarEstadoIncidencia,
+    eliminarIncidencia
 };
--- Proyecto/routes/incidencias.js
+++ Proyecto/routes/incidencias.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 const {
-    registrarIncidencia
+    registrarIncidencia,
+    cambiarEstadoIncidencia,
+    eliminarIncidencia
 } = require("../controllers/incidenciasController");
+
 router.post("/", registrarIncidencia);
+router.put("/:id/estado", cambiarEstadoIncidencia);
+router.delete("/:id", eliminarIncidencia);
 
 module.exports = router;
